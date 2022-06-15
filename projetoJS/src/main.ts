9d1575a15b69b737d06a78d3297c761762901e68
F8367174 Rafael Gama Palone <rafaelgpalone@bb.com.br>
2022-06-15 12:15:32 -0300

    usando reduce

--- projetoJS/src/main.ts
+++ projetoJS/src/main.ts
@@ -53,108 +53,111 @@ const retornaPlanetasFilme = async (numeroDoFilme: number) => {
 const retornaPersonagensAlturaFilme = async (
   altura: number,
   numeroDoFilme: number,
-  maior:boolean,
+  maior: boolean
 ) => {
   const response = await (
     await fetch(`https://swapi.dev/api/films/${numeroDoFilme}/`)
   ).json();
   const filme: Filme = response;
   for (const character of filme.characters) {
-      const response2 = await (await fetch(`${character}`)).json();
-      const personagem: Personagem = response2;
-      if(parseFloat(personagem.height) > altura && maior){
-        console.log(`${personagem.name} tem altura maior que ${altura}` );
-      }
-      else if(parseFloat(personagem.height) < altura && !maior){
-        console.log(`${personagem.name} tem altura menor que ${altura}`);
-      }
+    const response2 = await (await fetch(`${character}`)).json();
+    const personagem: Personagem = response2;
+    if (parseFloat(personagem.height) > altura && maior) {
+      console.log(`${personagem.name} tem altura maior que ${altura}`);
+    } else if (parseFloat(personagem.height) < altura && !maior) {
+      console.log(`${personagem.name} tem altura menor que ${altura}`);
+    }
   }
 };
 
 const retornaPersonagensPesoFilme = async (
   peso: number,
   numeroDoFilme: number,
-  maior:boolean,
+  maior: boolean
 ) => {
   const response = await (
     await fetch(`https://swapi.dev/api/films/${numeroDoFilme}/`)
   ).json();
   const filme: Filme = response;
   for (const character of filme.characters) {
-      const response2 = await (await fetch(`${character}`)).json();
-      const personagem: Personagem = response2;
-      if(parseFloat(personagem.mass) > peso && maior){
-        console.log(`${personagem.name} tem peso maior que ${peso}` );
-      }
-      else if(parseFloat(personagem.mass) < peso && !maior){
-        console.log(`${personagem.name} tem peso menor que ${peso}`);
-      }
+    const response2 = await (await fetch(`${character}`)).json();
+    const personagem: Personagem = response2;
+    if (parseFloat(personagem.mass) > peso && maior) {
+      console.log(`${personagem.name} tem peso maior que ${peso}`);
+    } else if (parseFloat(personagem.mass) < peso && !maior) {
+      console.log(`${personagem.name} tem peso menor que ${peso}`);
+    }
   }
 };
 
 const retornaPersonagemFilmeCor = async (
   cor: String,
-  numeroDoFilme: number,
+  numeroDoFilme: number
 ) => {
   const response = await (
     await fetch(`https://swapi.dev/api/films/${numeroDoFilme}/`)
   ).json();
   const filme: Filme = response;
   for (const character of filme.characters) {
-      const response2 = await (await fetch(`${character}`)).json();
-      const personagem: Personagem = response2;
-      if(personagem.skin_color === cor ){
-        console.log(`${personagem.name} tem a cor ${cor}` );
-      }
-      else if(personagem.skin_color != cor){
-        console.log(`${personagem.name} não tem a cor ${cor}`);
-      }
+    const response2 = await (await fetch(`${character}`)).json();
+    const personagem: Personagem = response2;
+    if (personagem.skin_color === cor) {
+      console.log(`${personagem.name} tem a cor ${cor}`);
+    } else if (personagem.skin_color != cor) {
+      console.log(`${personagem.name} não tem a cor ${cor}`);
+    }
   }
 };
 
-const velocidadeMediaFilme = async (numeroDoFilme: number)=>{
+const velocidadeMediaFilme = async (numeroDoFilme: number) => {
   const response = await (
     await fetch(`https://swapi.dev/api/films/${numeroDoFilme}/`)
   ).json();
+  
   const filme: Filme = response;
-  let velocidades: number = 0;
+  let veiculos: Veiculo[] = [];
   for (const vehicle of filme.vehicles) {
     const response2 = await (await fetch(`${vehicle}`)).json();
     const veiculo: Veiculo = response2;
-    velocidades += parseFloat(veiculo.max_atmosphering_speed);
+    veiculos = [...veiculos,veiculo]
   }
-  console.log(velocidades/filme.vehicles.length);
-}
+  const velocidade: number = veiculos.reduce((velocidade,itens)=> velocidade += parseFloat(itens.max_atmosphering_speed),0)
+  console.log(velocidade / filme.vehicles.length);
+};
 
-const populacaoMediaFilme = async (numeroDoFilme: number)=>{
+const populacaoMediaFilme = async (numeroDoFilme: number) => {
   const response = await (
     await fetch(`https://swapi.dev/api/films/${numeroDoFilme}/`)
   ).json();
   const filme: Filme = response;
-  let massa: number = 0;
+  let planetas: Planeta[] = [];
   for (const planet of filme.planets) {
     const response2 = await (await fetch(`${planet}`)).json();
     const planeta: Planeta = response2;
-    massa += parseFloat(planeta.population);
+    planetas = [...planetas,planeta]
   }
-  console.log(massa/filme.planets.length);
-}
+  const populacao: number = planetas.reduce((populacao,itens)=> populacao +=parseFloat(itens.population),0);
+  console.log(populacao / filme.planets.length);
+};
 
-const menu = async ()=>{
+const menu = async () => {
   let opcao = 0;
-  while(opcao!= 12){
-    opcao = parseInt( prompt(
-      `Selecione uma das opções \n1:retorna Personagens do Filme \n2:retorna Veiculos do Filme \n3:retorna Naves do Filme
+  while (opcao != 12) {
+    opcao = parseInt(
+      prompt(
+        `Selecione uma das opções \n1:retorna Personagens do Filme \n2:retorna Veiculos do Filme \n3:retorna Naves do Filme
 4:retorna Planetas do Filme \n5:retorna Personagens maiores do que a altura do Filme \n6:retorna Personagens menores do que a altura do Filme 
 7:retorna Personagens mais pesados do que o peso informado do Filme \n8:retorna Personagens mais leves do que o peso informado do Filme 
 9:retorna se o Personagem tem a cor informada \n10:Velocidade Media dos veiculos do filme \n11:População media dos planetas do filme
 12:Sair`
-    ));
-      
-    
-    switch(opcao){
+      )
+    );
+    let filme;
+    let altura;
+    let peso;
+    switch (opcao) {
       case 1:
-        let filme = prompt("digite o numero do filme");
+        filme = prompt("digite o numero do filme");
         await retornaPersonagensFilme(filme);
         break;
       case 2:
@@ -171,23 +174,23 @@ const menu = async ()=>{
         break;
       case 5:
         filme = prompt("digite o numero do filme");
-        let altura = prompt("digite a altura");
-        await retornaPersonagensAlturaFilme(altura,filme,true);
+        altura = prompt("digite a altura");
+        await retornaPersonagensAlturaFilme(altura, filme, true);
         break;
       case 6:
         filme = prompt("digite o numero do filme");
         altura = prompt("digite a altura");
-        await retornaPersonagensAlturaFilme(altura,filme,false);
+        await retornaPersonagensAlturaFilme(altura, filme, false);
         break;
       case 7:
         filme = prompt("digite o numero do filme");
-        let peso = prompt("digite o peso");
-        await retornaPersonagensPesoFilme(peso,filme,true);
+         peso = prompt("digite o peso");
+        await retornaPersonagensPesoFilme(peso, filme, true);
         break;
       case 8:
         filme = prompt("digite o numero do filme");
         peso = prompt("digite o peso");
-        await retornaPersonagensPesoFilme(peso,filme,false);
+        await retornaPersonagensPesoFilme(peso, filme, false);
         break;
       case 9:
         filme = prompt("digite o numero do filme");
@@ -200,15 +203,14 @@ const menu = async ()=>{
         break;
       case 11:
         filme = prompt("digite o numero do filme");
-        await populacaoMediaFilme(filme)
+        await populacaoMediaFilme(filme);
         break;
-      case 12: 
-        console.log("Saindo")
+      case 12:
+        console.log("Saindo");
         break;
     }
-
   }
-}
+};
 
 menu();
 //retornaPersonagensFilme(4);
